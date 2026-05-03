da6e0d14a0e80fd24ee9e1a9a6536c52f0985aae
Soldatov Serhii <soldatov.own@gmail.com>
2026-05-03 10:31:25 +0200

    correct urls to be under /api/

--- core/urls.py
+++ core/urls.py
@@ -14,11 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("search/", include("github_search.urls")),
-    path("users/", include("users.urls")),
+    path(
+        "api/",
+        include(
+            [
+                path("search/", include("github_search.urls")),
+                path("users/", include("users.urls")),
+            ]
+        ),
+    ),
 ]
