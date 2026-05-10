210e92567faf7e896c841b14cdc15e9b03a9037a
Soldatov Serhii <soldatov.own@gmail.com>
2026-05-10 10:33:34 +0200

    add django cors

--- core/settings.py
+++ core/settings.py
@@ -45,12 +45,14 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django_completion",
     "rest_framework",
+    "corsheaders",
     "users",
     "github_search",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
 
 GITHUB_TOKEN = env.str("GITHUB_TOKEN")
 
